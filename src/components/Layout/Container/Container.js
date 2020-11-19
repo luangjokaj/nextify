de0333cb163e31a26ff3e7847a9afd8947167629
Luan <luan@riangle.com>
2020-11-19 23:31:41 +0100

    👌 IMPROVE: Linting

--- src/components/Layout/Container/Container.js
+++ src/components/Layout/Container/Container.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { useTheme } from "@emotion/react";
 import * as styles from "./Container.styles";
 
 function Container({ children }) {
-	return <div css={styles.containerStyles}>{children}</div>;
+	const theme = useTheme();
+	return <div css={styles.containerStyles(theme)}>{children}</div>;
 }
 
 export { Container };
--- src/components/Layout/Container/Container.styles.js
+++ src/components/Layout/Container/Container.styles.js
@@ -1,9 +1,11 @@
-import { css } from "@emotion/react";
+import { css, useTheme } from "@emotion/react";
 
-const containerStyles = css`
+const containerStyles = (theme) => css`
 	max-width: 1400px;
 	margin: auto;
 	padding: 0 20px;
+	background: ${theme.colors.primary};
+	color: red;
 `;
 
 export { containerStyles };
