de05b22d3e07bf45f6cf7cd0ecdbbb16646bd32c
Luan <luan@riangle.com>
2020-11-21 23:00:20 +0100

    👌 IMPROVE: Container

--- src/components/Layout/Container/Container.js
+++ src/components/Layout/Container/Container.js
@@ -2,9 +2,9 @@ import React from "react";
 import { useTheme } from "@emotion/react";
 import * as styles from "./Container.styles";
 
-function Container({ children }) {
+function Container({ children, fluid }) {
 	const theme = useTheme();
-	return <div css={styles.containerStyles(theme)}>{children}</div>;
+	return <div css={styles.containerStyles(theme, fluid)}>{children}</div>;
 }
 
 export { Container };
--- src/components/Layout/Container/Container.styles.js
+++ src/components/Layout/Container/Container.styles.js
@@ -1,9 +1,19 @@
 import { css, useTheme } from "@emotion/react";
+import { mq, Breakpoints } from "../../../assets/styles/mq";
 
-const containerStyles = (theme) => css`
+const containerStyles = (theme, fluid) => css`
 	max-width: 1400px;
 	margin: auto;
-	padding: 0 20px;
+	padding: 0 ${theme.spacing.marginContainer.mobile};
+
+	${mq(Breakpoints.lg)} {
+		padding: 0 ${theme.spacing.marginContainer.desktop};
+	}
+
+	${fluid && css`
+		max-width: 100%;
+		widht: 100%;
+	`}
 `;
 
 export { containerStyles };
