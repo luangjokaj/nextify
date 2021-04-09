add9c7f78c63ca3fb16eba11230fbcf64e3eba4c
Luan Gjokaj <luan@riangle.com>
2021-04-09 15:45:33 +0200

    fix: font load

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "@emotion/react";
+import Head from "next/head";
 import { globalStyles } from "../assets/styles/globalStyles";
 import { theme } from "../assets/styles/theme";
 
@@ -6,6 +7,30 @@ function App({ Component, pageProps }) {
 	return (
 		<>
 			{globalStyles}
+			<Head>
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="true"
+				/>
+				<link
+					rel="preload"
+					as="style"
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+				/>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+					media="print"
+					onLoad="this.media='all'"
+				/>
+				<noscript>
+					<link
+						rel="stylesheet"
+						href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+					/>
+				</noscript>
+			</Head>
 			<ThemeProvider theme={theme}>
 				<Component {...pageProps} />
 			</ThemeProvider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,30 +5,7 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html>
-				<Head>
-					<link
-						rel="preconnect"
-						href="https://fonts.gstatic.com"
-						crossOrigin="true"
-					/>
-					<link
-						rel="preload"
-						as="style"
-						href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-					/>
-					<link
-						rel="stylesheet"
-						href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-						media="print"
-						onLoad="this.media='all'"
-					/>
-					<noscript>
-						<link
-							rel="stylesheet"
-							href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-						/>
-					</noscript>
-				</Head>
+				<Head />
 				<body>
 					<Main />
 					<NextScript />
