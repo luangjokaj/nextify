dcc2ab8025d4d094ce4a51dab199ca96fcc8e425
Luan Gjokaj <luan@riangle.com>
2021-04-09 14:16:51 +0200

    fix: load fonts once

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -47,31 +47,6 @@ const Page = ({
 			<link rel="icon" type="image/png" sizes="16x16" href={favIcon16} />
 			<link rel="shortcut icon" href={favIcon} />
 			<title>{title + " - Nextify"}</title>
-			<link
-				rel="preconnect"
-				href="https://fonts.gstatic.com"
-				crossOrigin="true"
-			/>
-			<link
-				rel="preload"
-				as="style"
-				href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-			/>
-			<link
-				rel="stylesheet"
-				href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-				media="print"
-				// Allows to load CSS asynchronously
-				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-				// @ts-ignore
-				onLoad="this.media='all'"
-			/>
-			<noscript>
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-				/>
-			</noscript>
 		</Head>
 		{children}
 	</React.Fragment>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,30 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html>
-				<Head />
+				<Head>
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin="true"
+					/>
+					<link
+						rel="preload"
+						as="style"
+						href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+					/>
+					<link
+						rel="stylesheet"
+						href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+						media="print"
+						onLoad="this.media='all'"
+					/>
+					<noscript>
+						<link
+							rel="stylesheet"
+							href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+						/>
+					</noscript>
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
