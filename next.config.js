d84ad305f39bef8ce737cfd77c310e464bec95d9
Luan Gjokaj <luan@riangle.com>
2021-06-16 16:29:16 +0200

    chore: update dependencies

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 const withPlugins = require("next-compose-plugins");
 const withBundleAnalyzer = require("@next/bundle-analyzer");
 
-const config = {
-	future: {
-		webpack5: true,
-	},
-};
+const config = {};
 
 const bundleAnalyzer = withBundleAnalyzer({
 	enabled: process.env.ANALYZE === "true",
