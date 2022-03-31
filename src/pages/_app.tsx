daf1a1a2097e8c411a01a9a6986aa81c35e50e5c
Luan Gjokaj <luan@riangle.com>
2022-04-01 00:52:58 +0200

    feat: update nextjs document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,40 +1,11 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import { ThemeProvider } from "@emotion/react";
-import Head from "next/head";
 import { globalStyles } from "../assets/styles/globalStyles";
 import { theme } from "../assets/styles/theme";
 
 function App({ Component, pageProps }) {
-	const [isLoaded, setIsLoaded] = useState(false);
-
-	useEffect(() => {
-		setIsLoaded(true);
-	}, []);
 	return (
 		<>
-			<Head>
-				<link
-					rel="preconnect"
-					href="https://fonts.gstatic.com"
-					crossOrigin="true"
-				/>
-				<link
-					rel="preload"
-					as="style"
-					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-				/>
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-					media={isLoaded ? "all" : "print"}
-				/>
-				<noscript>
-					<link
-						rel="stylesheet"
-						href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-					/>
-				</noscript>
-			</Head>
 			<ThemeProvider theme={theme}>
 				{globalStyles}
 				<Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,42 @@
-import React from "react";
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import React, { useEffect, useState } from "react";
+import { Html, Head, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
-	render() {
-		return (
-			<Html>
-				<Head />
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+function Document() {
+	const [isLoaded, setIsLoaded] = useState(false);
+	useEffect(() => {
+		setIsLoaded(true);
+	}, []);
+	return (
+		<Html>
+			<Head>
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="true"
+				/>
+				<link
+					rel="preload"
+					as="style"
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+				/>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+					media={isLoaded ? "all" : "print"}
+				/>
+				<noscript>
+					<link
+						rel="stylesheet"
+						href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+					/>
+				</noscript>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
 
-export default MyDocument;
+export default Document;
